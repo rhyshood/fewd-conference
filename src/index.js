5996dd559667393af3c549cf6fdbea2bf9a0e5d8
rhyshood <rhys.hoodcurran04@gmail.com>
2025-01-04 03:16:45 +0000

    Removed Unused Variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import Navigation from './components/Navigation';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
